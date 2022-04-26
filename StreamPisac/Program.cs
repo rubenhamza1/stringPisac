e5e6fedffa71be6acb7f5c82bf69f9e586fe1df0
rubenhamza1 <90246098+rubenhamza1@users.noreply.github.com>
2022-04-26 13:09:52 +0200

    Add files via upload

---
+++ StreamPisac/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StreamPisac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upišite ime: ");
+            string ime = Console.ReadLine();
+
+            Console.WriteLine("Upišite prezime: ");
+            string prezime = Console.ReadLine();
+
+            StreamWriter sw = new StreamWriter(
+                @"c:\prviDir\mojaDatoteka.txt");
+
+            sw.WriteLine("Ime: {0}", ime);
+            sw.WriteLine("Prezime: {0}", prezime);
+            sw.Close();
+
+
+            StreamReader sr = new StreamReader(
+                @"c:\prviDir\mojaDatoteka.txt");
+
+            while (!sr.EndOfStream)
+            {
+                Console.WriteLine(sr.ReadLine());
+            }
+
+            sr.Close();
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
